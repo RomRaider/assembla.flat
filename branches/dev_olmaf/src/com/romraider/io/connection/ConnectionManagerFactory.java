3e358c5a7e943ad5403be9ca708b311e1d291b38
Jared Gould <jared.gould@gmail.com>
2011-01-19 00:45:54 +0000

    git-svn-id: https://svn2.assembla.com/svn/romraider@317 38686702-15cf-42e4-a595-3071df8bf5ea

---
+++ branches/dev_olmaf/src/com/romraider/io/connection/ConnectionManagerFactory.java
@@ -0,0 +1,51 @@
+/*
+ * RomRaider Open-Source Tuning, Logging and Reflashing
+ * Copyright (C) 2006-2010 RomRaider.com
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+package com.romraider.io.connection;
+
+import com.romraider.io.j2534.api.J2534ConnectionManager;
+import com.romraider.io.serial.connection.SerialConnectionManager;
+import static com.romraider.util.proxy.Proxifier.proxy;
+import com.romraider.util.proxy.TimerWrapper;
+import org.apache.log4j.Logger;
+import static org.apache.log4j.Logger.getLogger;
+
+public final class ConnectionManagerFactory {
+    private static final Logger LOGGER = getLogger(ConnectionManagerFactory.class);
+    private static final boolean ENABLE_TIMER = false;
+
+    private ConnectionManagerFactory() {
+    }
+
+    public static ConnectionManager getManager(String portName, ConnectionProperties connectionProperties) {
+        ConnectionManager manager = manager(portName, connectionProperties);
+        if (ENABLE_TIMER) return proxy(manager, TimerWrapper.class);
+        return manager;
+    }
+
+    private static ConnectionManager manager(String portName, ConnectionProperties connectionProperties) {
+        try {
+            LOGGER.info("Trying J2534 connection...");
+            return new J2534ConnectionManager(connectionProperties);
+        } catch (Throwable t) {
+            LOGGER.info("J2534 connection not available [" + t.getClass().getName() + ": " + t.getMessage() + "], trying serial connection...");
+            return new SerialConnectionManager(portName, connectionProperties);
+        }
+    }
+}
---
+++ branches/dev_olmaf/src/com/romraider/logger/ecu/comms/reset/ResetManagerImpl.java
@@ -0,0 +1,58 @@
+/*
+ * RomRaider Open-Source Tuning, Logging and Reflashing
+ * Copyright (C) 2006-2010 RomRaider.com
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+package com.romraider.logger.ecu.comms.reset;
+
+import com.romraider.Settings;
+import com.romraider.logger.ecu.comms.io.connection.LoggerConnection;
+import static com.romraider.logger.ecu.comms.io.connection.LoggerConnectionFactory.getConnection;
+import com.romraider.logger.ecu.ui.MessageListener;
+import static com.romraider.util.ParamChecker.checkNotNull;
+import org.apache.log4j.Logger;
+
+public final class ResetManagerImpl implements ResetManager {
+    private static final Logger LOGGER = Logger.getLogger(ResetManagerImpl.class);
+    private final Settings settings;
+    private final MessageListener messageListener;
+
+    public ResetManagerImpl(Settings settings, MessageListener messageListener) {
+        checkNotNull(settings, messageListener);
+        this.settings = settings;
+        this.messageListener = messageListener;
+    }
+
+    public boolean resetEcu() {
+        try {
+            LoggerConnection connection = getConnection(settings.getLoggerProtocol(), settings.getLoggerPort(),
+                    settings.getLoggerConnectionProperties());
+            try {
+                messageListener.reportMessage("Sending ECU Reset...");
+                connection.ecuReset();
+                messageListener.reportMessage("Sending ECU Reset...done.");
+                return true;
+            } finally {
+                connection.close();
+            }
+        } catch (Exception e) {
+            messageListener.reportMessage("Unable to reset ecu - check correct serial port has been selected, cable is connected and ignition is on.");
+            LOGGER.error("Error sending ecu reset", e);
+            return false;
+        }
+    }
+}
